da559f7c4a1b77b0d2b5f9f33b2e5dd3d53773e2
Mohit Patel <pmohit645@gmail.com>
2024-08-17 17:33:47 +0530

    Update Login.jsx
    import correction

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { Label } from '@radix-ui/react-label'
 import React, { useEffect, useState } from 'react'
 import { Input } from './ui/input'
-import { Button } from './ui/button'
+import { Button, Camera } from './ui/button'
 import axios from 'axios'
 import { toast } from 'sonner'
 import { Link, useNavigate } from 'react-router-dom'
